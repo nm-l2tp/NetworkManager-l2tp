bb1a4cb5ddc7c5afde05c46ebda06ec6a0755072
dkosovic <doug@uq.edu.au>
2018-06-30 21:55:30 +1000

    pppd-plugin: use g_strlcpy()
    It's nicer but also doesn't annoy gcc 8: "error: ‘strncpy’ specified bound
    depends on the length of the source argument [-Werror=stringop-overflow=]"

--- src/nm-l2tp-pppd-plugin.c
+++ src/nm-l2tp-pppd-plugin.c
@@ -248,7 +248,6 @@ get_credentials (char *username, char *password)
 {
 	const char *my_username = NULL;
 	const char *my_password = NULL;
-	size_t len;
 	GVariant *ret;
 	GError *error = NULL;
 
@@ -279,20 +278,11 @@ get_credentials (char *username, char *password)
 
 	g_variant_get (ret, "(&s&s)", &my_username, &my_password);
 
-	if (my_username) {
-		len = strlen (my_username) + 1;
-		len = len < MAXNAMELEN ? len : MAXNAMELEN;
-
-		strncpy (username, my_username, len);
-		username[len - 1] = '\0';
-	}
+	if (my_username)
+		g_strlcpy (username, my_username, MAXNAMELEN);
 
 	if (my_password) {
-		len = strlen (my_password) + 1;
-		len = len < MAXSECRETLEN ? len : MAXSECRETLEN;
-
-		strncpy (password, my_password, len);
-		password[len - 1] = '\0';
+		g_strlcpy (password, my_password, MAXSECRETLEN);
 	}
 
 	g_variant_unref (ret);
