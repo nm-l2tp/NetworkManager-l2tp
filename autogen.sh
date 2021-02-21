219123de661dc16eb4f79b7ca7476fdc63a839d3
dkosovic <doug@uq.edu.au>
2021-02-22 08:06:59 +1000

    build: stop relying on intltool for i18n
    Recent gettext version can extract and merge back strings from and to
    various file formats, no need for intltool anymore.
    
    https://wiki.gnome.org/Initiatives/GnomeGoals/GettextMigration

--- autogen.sh
+++ autogen.sh
@@ -1,22 +1,32 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
+set -e
+
 srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
+if test -z "$srcdir"; then
+    srcdir=.
+fi
+
+olddir=`pwd`
+
 REQUIRED_AUTOMAKE_VERSION=1.9
 PKG_NAME=NetworkManager-l2tp
 
 (test -f $srcdir/configure.ac \
   && test -f $srcdir/auth-dialog/main.c) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level $PKG_NAME directory"
+    printf "**Error**: Directory "\`$srcdir\'" does not look like the" >&2
+    echo " top-level $PKG_NAME directory" >&2
     exit 1
 }
 
-(cd $srcdir;
-    autoreconf --install --symlink &&
-    intltoolize --force &&
-    autoreconf &&
-    ./configure --enable-maintainer-mode $@
-)
+cd $srcdir
+
+aclocal --install || exit 1
+autoreconf --verbose --force --install || exit 1
+
+cd $olddir
+if test -z "$NOCONFIGURE"; then
+    exec $srcdir/configure --enable-maintainer-mode --enable-more-warnings=error "$@"
+fi
 
